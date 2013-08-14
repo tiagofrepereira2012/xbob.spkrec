6ed5865e9cd6e962202b7a2af649330e8a86f20d
Elie Khoury <elie.khoury@idiap.ch>
2013-08-14 10:53:08 +0200

    added copyright for each file

--- spkrectool/script/manual_vad_conversion.py
+++ spkrectool/script/manual_vad_conversion.py
@@ -1,5 +1,20 @@
 #!bin/python
 # vim: set fileencoding=utf-8 :
+# Elie Khoury <Elie.Khoury@idiap.ch>
+#
+# Copyright (C) 2012-2013 Idiap Research Institute, Martigny, Switzerland
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import bob
 import time, imp
