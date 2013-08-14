bd0cfbf92f22c1d4bf496aa15cc58a32a3fed24d
Elie Khoury <elie.khoury@idiap.ch>
2013-08-14 09:40:05 +0200

    added a condition for non-speech files, this avoids errors in next steps

--- spkrectool/feature_extraction/Cepstral.py
+++ spkrectool/feature_extraction/Cepstral.py
@@ -80,6 +80,10 @@ class Cepstral:
       normalized_features = self.normalize_features(filtered_features)
     else:
       normalized_features = filtered_features
+    if normalized_features.shape[0] == 0:
+      print "Warning: no speech found in:", input_file
+      # But do not keep it empty!!! This avoids errors in next steps
+      normalized_features=numpy.array([numpy.zeros(len(features_mask))])
     return normalized_features
 
 
