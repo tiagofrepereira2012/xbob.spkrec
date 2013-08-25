0ad5420ed4e207170f9fe30633322175c7d9a689
Elie Khoury <elie.khoury@idiap.ch>
2013-08-25 15:35:07 +0200

    added config file for VoxForge database

---
+++ config/database/voxforge.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import xbob.db.voxforge
+
+# 0/ The database to use
+name = 'VoxForge'
+# put the full path to the database filelist
+db = xbob.db.voxforge.Database()
+protocol = 'VoxForge'
+
+# put the full path to the folder that contains the audio files
+wav_input_dir = "/idiap/temp/ekhoury/VoxForge/"
+wav_input_ext = ".wav"
+
