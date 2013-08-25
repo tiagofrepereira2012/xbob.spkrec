e02f00ecdcdc7df81a0c516516b08ab2e2a01d07
Elie Khoury <elie.khoury@idiap.ch>
2013-08-25 15:46:58 +0200

    cleaning config/grid/

--- config/grid/grid.py
+++ config/grid/grid.py
@@ -3,24 +3,25 @@
 # setup of the grid parameters
 
 # default queue used for training
-training_queue = { 'queue':'q1d', 'memfree':'8G' }
-
+training_queue = { 'queue':'q1d', 'memfree':'8G'}
+#training_queue = { 'queue':'q1d', 'memfree':'1G'}
 # number of images that one job should preprocess
-number_of_images_per_job = 100
-preprocessing_queue = {}
+number_of_images_per_job = 1000
+preprocessing_queue = {'queue':'q1d', 'memfree':'1G'}
+
 
 # number of features that one job should extract
-number_of_features_per_job = 100
-extraction_queue = {}
+number_of_features_per_job = 1000
+extraction_queue = {'queue':'q1d', 'memfree':'1G'}
 
 # number of features that one job should project
-number_of_projections_per_job = 100
-projection_queue = {}
+number_of_projections_per_job = 400
+projection_queue = {'queue':'q1d', 'memfree':'1G'}
 
 # number of models that should be enroled by one enrol job
-number_of_models_per_enrol_job = 10
-enrol_queue = {}
+number_of_models_per_enrol_job = 50
+enrol_queue = { 'queue':'q1d', 'memfree':'1G' }
 
 # number of models that one score computation should use
-number_of_models_per_score_job = 5
-score_queue = {}
+number_of_models_per_score_job = 10
+score_queue = { 'queue':'q1d', 'memfree':'1G' }
--- config/grid/very_demanding.py
+++ config/grid/very_demanding.py
@@ -3,22 +3,22 @@
 # setup of the grid parameters
 
 # default queue used for training
-#training_queue = { 'queue':'q1dm', 'memfree':'32G', 'pe_opt':'pe_mth 4', 'hvmem':'8G' }
-training_queue = {'queue':'q1d', 'hostname':'*hx5*'} #{ 'queue':'q1d', 'memfree':'1G'}
+training_queue = { 'queue':'q1dm', 'memfree':'64G', 'pe_opt':'pe_mth 8', 'hvmem':'8G' }
+#training_queue = {'queue':'q1d', 'hostname':'*hx5*'} #{ 'queue':'q1d', 'memfree':'1G'}
 # number of images that one job should preprocess
-number_of_images_per_job = 1000
+number_of_images_per_job = 500
 preprocessing_queue = {'queue':'q1d'} #{'queue':'q1d', 'memfree':'1G'}
 
 # number of features that one job should extract
-number_of_features_per_job = 1000
+number_of_features_per_job = 500
 extraction_queue = {'queue':'q1d'}
 
 # number of features that one job should project
-number_of_projections_per_job = 1000
+number_of_projections_per_job = 500
 projection_queue = {'queue':'q1d'}
 
 # number of models that should be enroled by one enrol job
-number_of_models_per_enrol_job = 10
+number_of_models_per_enrol_job = 20
 enrol_queue = {'queue':'q1d'} #{ 'queue':'q1d', 'memfree':'1G' }
 
 # number of models that one score computation should use
