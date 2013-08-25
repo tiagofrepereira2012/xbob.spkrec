43543fc6c3705ec81266872a53590484a59f1a09
Elie Khoury <elie.khoury@idiap.ch>
2013-08-25 15:44:45 +0200

    cleaning config/grid/

--- config/grid/very_demanding.py
+++ config/grid/very_demanding.py
@@ -3,24 +3,26 @@
 # setup of the grid parameters
 
 # default queue used for training
-training_queue = { 'queue':'q1wm', 'memfree':'32G', 'pe_opt':'pe_mth 8', 'hvmem':'8G' }
-
+#training_queue = { 'queue':'q1dm', 'memfree':'32G', 'pe_opt':'pe_mth 4', 'hvmem':'8G' }
+training_queue = {'queue':'q1d', 'hostname':'*hx5*'} #{ 'queue':'q1d', 'memfree':'1G'}
 # number of images that one job should preprocess
-number_of_images_per_job = 100
-preprocessing_queue = {'queue':'q1d', 'memfree':'1G'}
+number_of_images_per_job = 1000
+preprocessing_queue = {'queue':'q1d'} #{'queue':'q1d', 'memfree':'1G'}
 
 # number of features that one job should extract
-number_of_features_per_job = 100
-extraction_queue = {'queue':'q1d', 'memfree':'1G'}
+number_of_features_per_job = 1000
+extraction_queue = {'queue':'q1d'}
 
 # number of features that one job should project
-number_of_projections_per_job = 200
-projection_queue = {'queue':'q1d', 'memfree':'1G'}
+number_of_projections_per_job = 1000
+projection_queue = {'queue':'q1d'}
 
 # number of models that should be enroled by one enrol job
-number_of_models_per_enrol_job = 1
-enrol_queue = { 'queue':'q1d', 'memfree':'1G' }
+number_of_models_per_enrol_job = 10
+enrol_queue = {'queue':'q1d'} #{ 'queue':'q1d', 'memfree':'1G' }
 
 # number of models that one score computation should use
-number_of_models_per_score_job = 1
-score_queue = { 'queue':'q1d', 'memfree':'1G' }
+number_of_models_per_score_job = 50
+#score_queue = {'queue':'q1d', 'hostname':'*hx5*'} #{ 'queue':'q1d', 'memfree':'1G' }
+
+score_queue = {'queue':'q1d', 'io_big':True}
