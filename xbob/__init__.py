ab7d8e7f5c2a197618426837bd7f341a848ae0bf
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-08-31 11:32:22 +0200

    Fix imports

--- xbob/__init__.py
+++ xbob/__init__.py
@@ -16,7 +16,5 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import spkrec
-
 #see http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
 __import__('pkg_resources').declare_namespace(__name__)
--- xbob/spkrec/__init__.py
+++ xbob/spkrec/__init__.py
@@ -25,7 +25,3 @@ import feature_extraction
 import tools
 import utils
 import script
-
-#see http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
-__import__('pkg_resources').declare_namespace(__name__)
-
