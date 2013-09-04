ed6bd91b00b0dd82fc761e6a5f81c94ba20dcc6c
Elie Khoury <elie.khoury@idiap.ch>
2013-09-04 11:37:06 +0200

    go to version 0.0.a3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xbob.spkrec',
-    version='0.0.1a2',
+    version='0.0.1a3',
     description='Speaker recognition toolkit',
     url='https://pypi.python.org/pypi/xbob.spkrec',
     license='GPLv3',
